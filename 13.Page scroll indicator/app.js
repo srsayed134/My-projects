05da68fe3f0542d59b3b2edb5fb06b4621e7693c
srsayed134 <srsayed134@gmail.com>
2025-06-20 23:35:01 +0600

    13 project have done

--- 13.Page scroll indicator/app.js
+++ 13.Page scroll indicator/app.js
@@ -1,3 +1,24 @@
-let indicator = document.querySelector(".scroll-indicator .progress")
+let indicator = document.querySelector(".scroll-indicator .progress");
 
-console.log(indicator);
+//Full page scrollable height
+let totalscrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+console.log(`Total Height -` + totalscrollableHeight);
+
+
+window.addEventListener("scroll", function (){
+    //How far i scrolled now
+    let scrollingPosition = document.documentElement.scrollTop;
+    console.log(`My present height -` + scrollingPosition); 
+    
+    //the percentage we have scrolled
+    let scrollPercentage = (scrollingPosition / totalscrollableHeight) * 100;
+    console.log(`Scolling percentage -` + scrollPercentage);
+    
+    
+    //give width percentage to (.scroll-indicator .progress)
+    indicator.style.width = `${scrollPercentage}%`;
+});
+
+
+// For batter understand follow this - https://youtu.be/lwZTJ0TJTN8?si=aDsONFpaP_DyMJij 
+
