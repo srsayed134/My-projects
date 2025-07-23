9a713a4dc295374e32f5035751e0dacadb3fde76
srsayed134 <srsayed134@gmail.com>
2025-07-23 23:52:35 +0600

    Complete 14 no project

---
+++ 100 Projects/14.heart/app.js
@@ -0,0 +1,11 @@
+const grayHeart = document.querySelector(".gray-heart");
+const redHeart = document.querySelector(".red-heart");
+
+grayHeart.addEventListener("click", () => {
+    redHeart.classList.add("animation");
+    grayHeart.classList.add("fill-color");
+})
+grayHeart.addEventListener("click", () => {
+    redHeart.classList.add("animation");
+    grayHeart.classList.add("fill-color");
+})
