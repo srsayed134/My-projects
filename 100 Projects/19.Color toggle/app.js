5f6ba680773449a8f5f629e5302baf90f84eb082
srsayed134 <srsayed134@gmail.com>
2025-07-27 12:48:10 +0600

    Complete 18 and 19 project

---
+++ 100 Projects/19.Color toggle/app.js
@@ -0,0 +1,5 @@
+let toggler = document.getElementById("switch");
+
+toggler.addEventListener("click", () => {
+    toggler.checked === true ? document.body.style.backgroundColor = "black" : document.body.style.backgroundColor = "white"
+})
